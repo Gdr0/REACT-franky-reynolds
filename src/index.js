3cce151c44e56312b5c10953e94594f8c80230d2
Gabriele Di Rocco <gabriele.dirocco000@gmail.com>
2024-06-18 00:19:42 +0200

    Franky-Reynolds-REACT

--- src/index.js
+++ src/index.js
@@ -1,17 +1,73 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="container">
+      <div className="card">
+        <Avatar />
+        <div className="data">
+          <Intro />
+          {/* Should contain one Skill component
+        for each web dev skill that you have,
+      customized with props */}
+          <SkillList />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <img
+      className="avatar"
+      src="https://i.pinimg.com/474x/5d/53/86/5d53868a23946f6c65c664d7d62228c0.jpg"
+      alt="Frank Reynolds"
+    ></img>
+  );
+}
+function Intro() {
+  return (
+    <div>
+      <h1>Frank Reynols</h1>
+      <p>
+        Entrepenuer, Finicial Wizard, Co-owner of Paddy's Pub and mostly a Man
+        of The People. Also known as the Worthog, Trashman or Onglo Gablogian.
+      </p>
+    </div>
+  );
+}
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill name="Toe Knife Master" color="red" />
+      <Skill name="Tax Evasion" color="green" />
+      <Skill name="Scheeming" color="purple" />
+      <Skill name="Master Manipulator" color="yellow" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      {props.name}
+    </div>
+  );
+}
+
+//
+//
+//
+//
+//
+//
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
